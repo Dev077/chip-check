4e7fac66a11d3636160d2721de29e5a72f5bfd48
Ian Titor <ianpatricktan@gmail.com>
2026-05-18 16:25:34 -0400

    fixed debug sa to be updated with changes

--- placer/debug_sa.py
+++ placer/debug_sa.py
@@ -1,8 +1,8 @@
 import torch
 import random
-from src.annealing_placer import AnnealingPlacer
+from placer.annealer import AnnealerPlacer
 from macro_place.loader import load_benchmark_from_dir
 
 benchmark, _ = load_benchmark_from_dir("external/MacroPlacement/Testcases/ICCAD04/ibm01")
-placer = AnnealingPlacer(iterations=20)
+placer = AnnealerPlacer(iterations=20)
 placer.place(benchmark)
